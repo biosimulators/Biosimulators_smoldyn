57cfa9b1df12c6f6b4cf05cba6dfbf5d707e1213
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-08-26 11:42:59 -0400

    updated setup for common config as well as ignore for files

--- setup.py
+++ setup.py
@@ -1,20 +1,57 @@
-from setuptools import setup, find_packages
+import re
+import setuptools
+import subprocess
+import sys
+import os
+import pkg_utils
+
 from biosimulators_smoldyn import __version__
 
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# set dirs 
+name = 'biosimulators_smoldyn'
+dirname = os.path.dirname(__file__)
 
 
-setup(
-    name='Biosimulators_smoldyn',
+# set descriptions
+with open("README.md", "r") as readme:
+    description = readme.read()
+    # Patch the relative links to absolute URLs that will work on PyPI.
+    description2 = re.sub(
+        r']\(([\w/.-]+\.png)\)',
+        r'](https://github.com/biosimulators/Biosimulators_smoldyn/raw/main/\1)',
+        description)
+    long_description = re.sub(
+        r']\(([\w/.-]+)\)',
+        r'](https://github.com/biosimulators/Biosimulators_smoldyn/blob/main/\1)',
+        description2)
+
+
+# install package
+setuptools.setup(
+    name=name,
     version=__version__,
-    packages=find_packages(),
-    url='https://github.com/biosimulators/Biosimulators_simularium',
-    author='Alexander Patrie/BioSimulators Team',
-    author_email='info@biosimulators.org',
-    description='A Python tool for converting Biosimulators spatial simulation outputs into a Simularium-compliant format.',
+    description=("BioSimulators-compliant command-line interface to "
+                 "the Smoldyn simulation program <https://github.com/ssandrews/Smoldyn>."),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    entry_points={'console_scripts': []}
+    url="https://github.com/biosimulators/Biosimulators_Smoldynå",
+    download_url="https://github.com/biosimulators/Biosimulators_Smoldyn",
+    author='Center for Reproducible Biomedical Modeling',
+    author_email="info@biosimulators.org",
+    license="MIT",
+    keywords=['BioSimulators', 'systems biology', 'computational biology', 'mathematical model',
+              'kinetic model', 'simulation', 'stochastic', 'spatial', 'SED-ML', 'COMBINE', 'OMEX'],
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    entry_points={
+        'console_scripts': [
+            'biosimulators-amici = biosimulators_smoldyn.__main__:main',
+        ],
+    },
 )
